e80eeb761b1f4a239e071b452ea7630573da94ac
Mikaze Yoshizawa <mikazeyoshizawa@MikazenoMacBook-Air.local>
2022-04-20 11:43:59 +0900

    id追加しました

--- src/main/java/jp/co/seattle/library/service/BooksService.java
+++ src/main/java/jp/co/seattle/library/service/BooksService.java
@@ -33,7 +33,7 @@ public class BooksService {
 
         // TODO 取得したい情報を取得するようにSQLを修正
         List<BookInfo> getedBookList = jdbcTemplate.query(
-                "select title, author, publisher, publish_date, thumbnail_url from books order by title;",
+                "select id, title, author, publisher, publish_date, thumbnail_url from books order by title;",
                 new BookInfoRowMapper());
 
         return getedBookList;

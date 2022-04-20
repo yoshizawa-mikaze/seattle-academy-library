d91166cf848af466a7747b119ec9f9f338a6719e
Mikaze Yoshizawa <mikazeyoshizawa@MikazenoMacBook-Air.local>
2022-04-20 11:21:29 +0900

    BookInfoRowMapperとBooksServiceの再修正

--- src/main/java/jp/co/seattle/library/rowMapper/BookInfoRowMapper.java
+++ src/main/java/jp/co/seattle/library/rowMapper/BookInfoRowMapper.java
@@ -17,7 +17,6 @@ public class BookInfoRowMapper implements RowMapper<BookInfo> {
         BookInfo bookInfo = new BookInfo();
 
         // bookInfoの項目と、取得した結果(rs)のカラムをマッピングする
-        bookInfo.setBookId(rs.getInt("id"));
         bookInfo.setTitle(rs.getString("title"));
         bookInfo.setAuthor(rs.getString("author"));
         bookInfo.setPublisher(rs.getString("publisher"));

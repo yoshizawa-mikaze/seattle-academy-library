76a312c6cdc925963198c4f9e00ef6b60e1fccf0
Mikaze Yoshizawa <mikazeyoshizawa@MikazenoMacBook-Air.local>
2022-05-09 17:35:27 +0900

    タスク9作成

---
+++ src/main/java/jp/co/seattle/library/controller/RentBookController.java
@@ -0,0 +1,66 @@
+package jp.co.seattle.library.controller;
+
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import jp.co.seattle.library.dto.BookDetailsInfo;
+import jp.co.seattle.library.service.BooksService;
+import jp.co.seattle.library.service.RentBooksService;
+
+/**
+ * 
+ * 貸出コントローラー
+ *
+ */
+@Controller /** APIの入り口 */
+public class RentBookController {
+	final static Logger logger = LoggerFactory.getLogger(RentBookController.class);
+	
+	
+	@Autowired
+	private RentBooksService rentBooksService;
+	
+	@Autowired
+	private BooksService booksService;
+	/**
+	 * 
+	 * 貸出処理
+	 * 
+	 */
+	@RequestMapping(value = "/rentBook", method = RequestMethod.POST)
+	public String rentBook(Locale locale,
+			@RequestParam("bookId") int bookId,
+//			@RequestParam("title") String title,
+//            @RequestParam("author") String author,
+//            @RequestParam("publisher") String publisher,
+//            @RequestParam("publish_date") String publishDate,
+//            @RequestParam("isbn") String isbn,
+//            @RequestParam("texts") String texts,
+//            @RequestParam("thumbnail") MultipartFile file,
+            Model model) {
+		BookDetailsInfo bookInfo = new BookDetailsInfo();
+		int rentedBookId = rentBooksService.getRentBookInfo(bookId);
+		
+	//貸出テーブルに書籍が存在するかチェック
+		if(rentedBookId == 0) {
+			rentBooksService.insertRentBook(bookId);
+			System.out.println("b");
+			
+		} else {
+			
+			model.addAttribute("error", "貸出中です。");
+			System.out.println("a");
+		}
+		
+		model.addAttribute("bookDetailsInfo", booksService.getBookInfo(bookId));
+		return "details";
+	}
+}
---
+++ src/main/java/jp/co/seattle/library/service/RentBooksService.java
@@ -0,0 +1,46 @@
+package jp.co.seattle.library.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+
+/**
+ * 
+ * 書籍貸出サービス
+ *
+ */
+@Service
+public class RentBooksService {
+	final static Logger logger = LoggerFactory.getLogger(BooksService.class);
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+    
+    /**
+     * 書籍IDに紐づく書籍詳細情報を貸出リストから取得する
+     *
+     * @param bookId 書籍ID
+     * @return 書籍情報
+     */
+    public int getRentBookInfo(int bookId) {
+    	String sql = "SELECT COUNT(*) FROM rent_books where book_id = ?";
+
+    	int rentId = jdbcTemplate.queryForObject(sql, int.class,bookId);
+  				
+        return rentId;
+    }
+    /**
+     * 
+     * 書籍を貸出テーブルに登録する
+     * 
+     * @param bookId 書籍ID
+     * 
+     */
+    public void insertRentBook(int bookId) {
+    	String sql = "INSERT INTO rent_books (book_id) VALUES (?) ;";
+    	
+    	jdbcTemplate.update(sql,bookId);
+    }
+    
+}

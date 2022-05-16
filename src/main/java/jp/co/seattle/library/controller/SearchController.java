022340cc456b4db65bd26c67eb7e3636f450cd6c
Mikaze Yoshizawa <mikazeyoshizawa@MikazenoMacBook-Air.local>
2022-05-16 16:26:46 +0900

    タスク13の作成

---
+++ src/main/java/jp/co/seattle/library/controller/SearchController.java
@@ -0,0 +1,42 @@
+package jp.co.seattle.library.controller;
+
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import jp.co.seattle.library.service.BooksService;
+
+
+/**
+ * 
+ *　検索コントローラー
+ *
+ */
+@Controller /** APIの入り口 */
+public class SearchController {
+	final static Logger logger = LoggerFactory.getLogger(SearchController.class);
+	
+	@Autowired
+	private BooksService booksService;
+	/**
+	 * 
+	 * 貸出処理
+	 * 
+	 */
+	@RequestMapping(value = "/searchBook", method = RequestMethod.POST)
+	public String searchBook(Locale locale,
+			@RequestParam("title") String title,Model model) {
+		booksService.searchBook(title);
+		model.addAttribute("bookList",booksService.searchBook(title));
+		return "home";
+	}
+	
+	
+}

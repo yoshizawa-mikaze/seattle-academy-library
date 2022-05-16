857b807fae0a53b84b19561752f900e76af87358
Mikaze Yoshizawa <mikazeyoshizawa@MikazenoMacBook-Air.local>
2022-05-16 11:24:08 +0900

    カラム名を変更しました

--- src/main/java/jp/co/seattle/library/rowMapper/BookDetailsInfoRowMapper.java
+++ src/main/java/jp/co/seattle/library/rowMapper/BookDetailsInfoRowMapper.java
@@ -25,7 +25,7 @@ public class BookDetailsInfoRowMapper implements RowMapper<BookDetailsInfo> {
         bookDetailsInfo.setThumbnailUrl(rs.getString("thumbnail_url"));
         bookDetailsInfo.setIsbn(rs.getString("isbn"));
         bookDetailsInfo.setTexts(rs.getString("texts"));
-        bookDetailsInfo.setStatus(rs.getString("貸出ステータス"));
+        bookDetailsInfo.setStatus(rs.getString("status"));
         return bookDetailsInfo;
     }
 
